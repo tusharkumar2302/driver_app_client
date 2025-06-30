80b6f78d5ff3890601f480236218aa82dcd722fe
tusharkumar2302 <tusharkumar27864@gmail.com>
2025-06-30 19:20:53 +0530

    feat: add splash screen logic to show SplashScreen component for 2 seconds on app launch

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
+import React, { useEffect, useState } from 'react';
 import { AuthProvider } from './src/context/AuthContext';
-import AppNavigator  from './src/navigation/AppNavigator';
+import AppNavigator from './src/navigation/AppNavigator';
+import SplashScreen from './src/screens/auth/Splash Screen/SplashScreen';
 
 export default function App() {
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setShowSplash(false), 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <AuthProvider>
-      <AppNavigator />
+      {showSplash ? <SplashScreen /> : <AppNavigator />}
     </AuthProvider>
   );
 }
