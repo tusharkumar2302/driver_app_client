47b496e7133b0d4ebf6dbc8a35adc3e732b4fc79
atulj10 <atulanandj10@gmail.com>
2025-06-30 15:51:06 +0530

    SignUpScreen completed

--- src/screens/auth/SignUp Screen/SignUpScreen.tsx
+++ src/screens/auth/SignUp Screen/SignUpScreen.tsx
@@ -12,61 +12,73 @@ import EyeClosed from '../../../assets/images/icons/EyeClosed';
 import Eye from '../../../assets/images/icons/Eye';
 import { AUTH_STRINGS } from '../../../constants/authStrings';
 import { CustomButton } from '../../../components/common/CustomButton';
+import AuthLayout from '../../../layouts/AuthLayout';
 
 const SignUpScreen = () => {
   const [visible, setVisible] = useState<boolean>(true);
-  return (
-    <View>
-      <View style={GlobalStyles.header}>
-        <ArrowRight width={28} height={28} />
-        <CustomText color={COLORS.white} variant="h2" weight="bold">
-          {AUTH_STRINGS.signup.title}
-        </CustomText>
-      </View>
-      <View style={signUpStyles.inputContainer}>
-        <CustomInput
-          leftIcon={<User width={18} height={18} />}
-          label={AUTH_STRINGS.signup.enterName}
-          placeholder={AUTH_STRINGS.login.placeholder}
-        />
-        <CustomInput
-          leftIcon={<Email width={16} height={16} />}
-          label={AUTH_STRINGS.login.enterEmail}
-          placeholder={AUTH_STRINGS.login.placeholder}
-        />
-        <CustomInput
-          leftIcon={<Lock color={COLORS.disabled} width={18} height={18} />}
-          rightIcon={
-            <Pressable
-              onPress={() => {
-                setVisible(prev => !prev);
-              }}
-            >
-              {!visible ? (
-                <Eye color={COLORS.disabled} width={18} height={18} />
-              ) : (
-                <EyeClosed color={COLORS.disabled} width={18} height={18} />
-              )}
-            </Pressable>
-          }
-          secureTextEntry={visible}
-          label={AUTH_STRINGS.login.enterPassword}
-          placeholder={AUTH_STRINGS.login.placeholder}
-        />
-        <CustomInput
-          leftIcon={<Lock color={COLORS.disabled} width={18} height={18} />}
-          label={AUTH_STRINGS.signup.confirmPassword}
-          placeholder={AUTH_STRINGS.login.placeholder}
-          secureTextEntry={true}
-        />
-        <CustomText align="right" color={COLORS.white}>
-          {AUTH_STRINGS.forgotPassword.title} ?
-        </CustomText>
-      </View>
-      <View style={signUpStyles.button}>
-        <CustomButton>{AUTH_STRINGS.signup.signUp}</CustomButton>
-      </View>
+  const customHeader = (
+    <View style={GlobalStyles.header}>
+      <ArrowRight width={28} height={28} />
+      <CustomText color={COLORS.white} variant="h2" weight="bold">
+        {AUTH_STRINGS.signup.title}
+      </CustomText>
     </View>
+  );
+
+  const customFooter = (
+    <CustomText color={COLORS.disabled} align="center">
+      Already have an account?&nbsp;
+      <CustomText color={COLORS.white}>Login</CustomText>
+    </CustomText>
+  );
+  return (
+    <AuthLayout header={customHeader} footer={customFooter}>
+      <View>
+        <View style={signUpStyles.inputContainer}>
+          <CustomInput
+            leftIcon={<User width={18} height={18} />}
+            label={AUTH_STRINGS.signup.enterName}
+            placeholder={AUTH_STRINGS.login.placeholder}
+          />
+          <CustomInput
+            leftIcon={<Email width={16} height={16} />}
+            label={AUTH_STRINGS.login.enterEmail}
+            placeholder={AUTH_STRINGS.login.placeholder}
+          />
+          <CustomInput
+            leftIcon={<Lock color={COLORS.disabled} width={18} height={18} />}
+            rightIcon={
+              <Pressable
+                onPress={() => {
+                  setVisible(prev => !prev);
+                }}
+              >
+                {!visible ? (
+                  <Eye color={COLORS.disabled} width={18} height={18} />
+                ) : (
+                  <EyeClosed color={COLORS.disabled} width={18} height={18} />
+                )}
+              </Pressable>
+            }
+            secureTextEntry={visible}
+            label={AUTH_STRINGS.login.enterPassword}
+            placeholder={AUTH_STRINGS.login.placeholder}
+          />
+          <CustomInput
+            leftIcon={<Lock color={COLORS.disabled} width={18} height={18} />}
+            label={AUTH_STRINGS.signup.confirmPassword}
+            placeholder={AUTH_STRINGS.login.placeholder}
+            secureTextEntry={true}
+          />
+          <CustomText align="right" color={COLORS.white}>
+            {AUTH_STRINGS.forgotPassword.title} ?
+          </CustomText>
+        </View>
+        <View style={signUpStyles.button}>
+          <CustomButton>{AUTH_STRINGS.signup.signUp}</CustomButton>
+        </View>
+      </View>
+    </AuthLayout>
   );
 };
 
