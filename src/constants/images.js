8d917cccc5754b7ccbf287b59e0f90159a36bd4d
tusharkumar2302 <tusharkumar27864@gmail.com>
2025-06-30 19:22:05 +0530

    feat: add logo image import to constants/images

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,2 +1,3 @@
 export const ForgotPassword1_img = require('../assets/images/Forgot-Password-1.png');
 export const ForgotPassword2_img = require('../assets/images/Forgot-Password-2.png');
+export const logo_img = require('../assets/images/logos/driver-app-logo.png');
