7a718b28f1cc023e4cadfd1a0f0641fbf5c6d3bd
atulj10 <atulanandj10@gmail.com>
2025-07-02 16:42:59 +0530

    Booking screen Completed

--- src/screens/bookings/Booking.tsx
+++ src/screens/bookings/Booking.tsx
@@ -1,14 +1,81 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, View, FlatList } from 'react-native';
+import React, { useState, useMemo } from 'react';
+import { bookings } from '../../data/bookings';
+import { CustomButton } from '../../components/common/CustomButton';
+import { GlobalStyles } from '../../styles/globalStyles';
+import { COLORS } from '../../constants/colors';
+import BookingCard from '../../components/BookingCard';
 
 const Booking = () => {
+  const [type, setType] = useState<'Completed' | 'Upcoming'>('Completed');
+
+  const upcomingBookings = useMemo(
+    () =>
+      bookings.filter(
+        b => b.status === 'Allotted' || b.status === 'Not-Allotted',
+      ),
+    [],
+  );
+
+  const completedBookings = useMemo(
+    () =>
+      bookings.filter(
+        b => b.status === 'Cancelled' || b.status === 'Completed',
+      ),
+    [],
+  );
+
+  const data = type === 'Completed' ? completedBookings : upcomingBookings;
+
   return (
-    <View>
-      <Text>Booking</Text>
+    <View style={{ flex: 1, marginTop: 20 }}>
+      <View style={[GlobalStyles.row, BookingStyles.triggerContainer]}>
+        <CustomButton
+          variant={type === 'Completed' ? 'primary' : 'text'}
+          style={BookingStyles.trigger}
+          onPress={() => setType('Completed')}
+        >
+          Completed
+        </CustomButton>
+        <CustomButton
+          variant={type === 'Upcoming' ? 'primary' : 'text'}
+          style={BookingStyles.trigger}
+          onPress={() => setType('Upcoming')}
+        >
+          Upcoming
+        </CustomButton>
+      </View>
+
+      {/* list */}
+      <FlatList
+        data={data}
+        keyExtractor={item => String(item.id)}
+        renderItem={({ item }) => (
+          <BookingCard
+            address={item.location.address}
+            city={item.location.city}
+            state={item.location.state}
+            slot={item.slot}
+            status={item.status}
+            price={item.cost.total}
+          />
+        )}
+        contentContainerStyle={{ padding: 12, gap: 20, paddingVertical: 32 }}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default Booking
+export default Booking;
 
-const styles = StyleSheet.create({})
+const BookingStyles = StyleSheet.create({
+  triggerContainer: {
+    padding: 10,
+    borderRadius: 8,
+    backgroundColor: COLORS.backgroundSecondary,
+  },
+  trigger: {
+    flex: 1,
+  },
+});
