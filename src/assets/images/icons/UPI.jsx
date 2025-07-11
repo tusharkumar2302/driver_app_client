ab2d94b5e2967bc76d400511e9084210ffb8e685
atulj10 <atulanandj10@gmail.com>
2025-07-11 22:59:31 +0530

    Added strings and heading change

--- src/assets/images/icons/UPI.jsx
+++ src/assets/images/icons/UPI.jsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import Svg, { Path, Defs, Pattern, Use, Image } from "react-native-svg"
-const SvgComponent = (props) => (
+const UPI = (props) => (
   <Svg
     xmlns="http://www.w3.org/2000/svg"
     xmlnsXlink="http://www.w3.org/1999/xlink"
@@ -31,4 +31,4 @@ const SvgComponent = (props) => (
     </Defs>
   </Svg>
 )
-export default SvgComponent
+export default UPI
--- src/constants/paymentStrings.js
+++ src/constants/paymentStrings.js
@@ -1,12 +1,15 @@
 export const PAYMENT_STRINGS = {
+  title: 'Payment Mode',
+  subtitle: 'Choose payment method',
   methods: {
-    cash: "Pay Cash",
-    upi: "UPI",
-    card: "Debit or Credit card"
+    cash: 'Pay Cash',
+    upi: 'UPI',
+    card: 'Debit or Credit card',
   },
+  savedTitle: 'Saved card',
   breakdown: {
-    rideCharge: "Ride Charge",
-    insurance: "Insurance",
-    platformFee: "Platform fee"
-  }
+    rideCharge: 'Ride Charge',
+    insurance: 'Insurance',
+    platformFee: 'Platform fee',
+  },
 };
