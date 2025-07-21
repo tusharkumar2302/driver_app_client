e2323816a95a4446daefe2bb9e5964a31ab0aa5e
atulj10 <atulanandj10@gmail.com>
2025-07-21 19:03:03 +0530

    Dummy saved location data and adjustment in the icon

---
+++ src/data/locations.js
@@ -0,0 +1,32 @@
+export const locations = [
+  {
+    id: '1',
+    title: 'Home',
+    address: 'Sector 18, Near DLF Cyber Hub, Gurugram, Haryana',
+  },
+  {
+    id: '2',
+    title: 'Work',
+    address: 'Salt Lake Sector V, Kolkata, West Bengal',
+  },
+  {
+    id: '3',
+    title: 'Parents',
+    address: 'Ballygunge, Kolkata, West Bengal',
+  },
+  {
+    id: '4',
+    title: 'Friend’s Place',
+    address: 'Powai, Mumbai, Maharashtra',
+  },
+  {
+    id: '5',
+    title: 'Gym',
+    address: 'Banashankari, Bengaluru, Karnataka',
+  },
+  {
+    id: '6',
+    title: 'Cafe Spot',
+    address: 'Connaught Place, New Delhi',
+  },
+];
